6aa83e8ad4f260034da4314a26558be30be11b9a
hjwenhai <hjwenhai@163.com>
2019-03-11 20:53:23 +0800

    xiu gai 1

--- TestGitHub_3/Program.cs
+++ TestGitHub_3/Program.cs
@@ -10,6 +10,8 @@ namespace TestGitHub_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
         }
     }
 }
